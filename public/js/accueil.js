cf2933c0023c2a952f908f51081aa6651478cdb7
AnassRh <anas.rach@gmail.com>
2013-11-01 18:38:27 -0100

    Storage du nom d'utilistateur

--- public/js/accueil.js
+++ public/js/accueil.js
@@ -2,8 +2,24 @@
   'use strict'; 
 
 	function nouveauJoueur(){
-		console.log("TODO");		
+	var nom = $("#inputNom").val();
+
+	$.ajax({
+		type: "POST",
+		url: "/nouveauNom/" +nom
+	}).done(function(msg) {
+		if(msg=="failed")
+		console.debug("Erreur existe deja");
+		else
+	
+			console.debug( "success : un nouveau joueur a été crée avec le nom : "+ nom );
+			localStorage['nomJoueur'] = nom;
+		
+	});
+		console.log("OK Click");		
 	};
 
 	$('#nom a').click(nouveauJoueur);
 })();
+
+
