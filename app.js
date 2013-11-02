feec22ada78f60081c8e59fa3a095d501d932a86
AnassRh <anas.rach@gmail.com>
2013-11-02 12:44:59 -0100

    Ajout de Socket.io

--- app.js
+++ app.js
@@ -67,13 +67,7 @@ var server = http.createServer(app);
 
 var fs = require('fs');
  
-// Chargement du fichier index.html affiché au client
-var server = http.createServer(function(req, res) {
-    fs.readFile('./index.html', 'utf-8', function(error, content) {
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(content);
-    });
-});
+
  
 // Chargement de socket.io
 var io = require('socket.io').listen(server);
